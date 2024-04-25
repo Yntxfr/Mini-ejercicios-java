43fa044a05c01618cb620966639adf3dbb95f6a1
Yntxfr <yntxfr71@pm.me>
2024-04-25 15:55:06 +0200

    quinto commit

---
+++ src/Main5.java
@@ -0,0 +1,31 @@
+// Nombre alumno: Alex Fernandez
+// Mini Ejercicios Java
+
+
+
+/*
+5. Array y Bucles:
+Crea un array de enteros y usa un bucle para sumar todos los valores del array.
+Modifica el array anterior para que imprima todos los valores al revés.
+Utiliza un bucle `for-each` para buscar el mayor número en el array.
+Desarrolla un programa que compare dos arrays de enteros y determine si son idénticos.
+Implementa un programa que encuentre el número más pequeño y el más grande en un array.
+ */
+
+
+import java.util.Scanner;  // Importar la clase Scanner
+
+public class Main5 {
+    public static void main(String[] args) {
+        // Crea un array de enteros y usa un bucle para sumar todos los valores del array
+        int[] myNum = {10, 20, 30, 40};
+
+        // Modifica el array anterior para que imprima todos los valores al revés
+
+        // Utiliza un bucle `for-each` para buscar el mayor número en el array
+
+        // Desarrolla un programa que compare dos arrays de enteros y determine si son idénticos
+    }
+
+
+}
